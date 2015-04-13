0fbc8c312de03cce631eb418643a82cfc83e1d2e
Justin Clift <justin@gluster.org>
2015-04-13 22:05:41 +0100

    Improved debugging message, added exit()s on error

--- collect_latest_stats.py
+++ collect_latest_stats.py
@@ -7,7 +7,7 @@ Needs code added to grab total # of downloads per day (when available).
 Requires the SQLite3 Python module
 """
 
-import os, os.path, ConfigParser
+import os, os.path, sys, ConfigParser
 import sqlite3
 import requests
 import json
@@ -44,7 +44,8 @@ for project in config.sections():
     api_page = requests.get(api_url, verify=True)
     if api_page.status_code != 200:
         print 'Error retrieving API page {0}'.format(api_url)
-        print '  Status code {0} : {1}'.format(api_page.status_code)
+        print '  Status code {0}'.format(api_page.status_code)
+        sys.exit(1)
     api_data = json.loads(api_page.text)
 
     # Extract the number of watchers, stars, and forks
@@ -57,13 +58,15 @@ for project in config.sections():
     commits_page = requests.get(commits_url, verify=True)
     if commits_page.status_code != 200:
         print 'Error retrieving Commit count page {0}'.format(commits_url)
-        print '  Status code {0} : {1}'.format(commits_page.status_code)
+        print '  Status code {0}'.format(commits_page.status_code)
+        sys.exit(2)
     commits_data = json.loads(commits_page.text)
     commits_count = len(commits_data)
 
     # Print the results to stdout
     if debug:
-        print 'watchers: {0}\tstars: {1}\tforks: {2}'.format(watchers, stars, forks)
+        print '{0} - commits: {1}\twatchers: {2}\tstars: {3}\tforks: {4}\n'.format(project, commits_count,
+                                                                                   watchers, stars, forks)
 
     # Add the results to the database
     sql = ('INSERT INTO social_stats (project, time_stamp, watchers, stars, forks, commits) VALUES '
