6c5f459eb49b6788a26494443ca3b92bd77c4bf8
Paul Cuzner <pcuzner@redhat.com>
2015-05-22 16:56:22 +1200

    reformated github requests and used datetime module for date/time handling

--- collect_latest_stats.py
+++ collect_latest_stats.py
@@ -1,105 +1,131 @@
 #!/usr/bin/env python
+
 """Collect the latest social stats for Gluster projects
 
 Grabs watchers, stars, forks, # of commits per day, and total number of downloads (ever).
 
+Known Issues
+1. uses anonymous github interaction which is ratelimited to 60 calls from the same IP per hour - so this won't scale
+   with the gluster github repos
+
 Requires the SQLite3 Python module
 """
 
-import os, os.path, sys, ConfigParser
+import os.path
+import ConfigParser
 import sqlite3
-import requests
 import json
+import datetime
+import requests
+
 
 # Enable this to have the results printed to stdout
 debug = True
 
-# Read the GitHub projects from the ./config file
-base_path = os.path.dirname(os.path.realpath(__file__))
-config_file_path = os.path.join(base_path, 'config')
-config = ConfigParser.SafeConfigParser()
-config.read(config_file_path)
 
-# Open the ./db/project_stats.db SQLite3 database
-db_path = os.path.join(base_path, 'db/project_stats.db')
-conn = sqlite3.connect(db_path)
+class GitHubRequest(object):
+    github_api_url = 'https://api.github.com/repos'
 
-# Connect to the database
-c = conn.cursor()
+    def __init__(self):
+        self.url = ''
+        self.response_ok = True
+        self.api_data = []
 
-# Create the SQLite3 table to store the info, if it's not already present
-sql = ('CREATE TABLE IF NOT EXISTS social_stats (project TEXT, time_stamp TEXT, '
-       'watchers INTEGER, stars INTEGER, forks INTEGER, commits INTEGER, downloads INTEGER)')
-c.execute(sql)
-conn.commit()
+    def get_stats(self, request_type, description):
+        self.url = '/'.join((GitHubRequest.github_api_url, request_type))
+        api_page = requests.get(self.url, verify=True)
+        if api_page.status_code == 200:
+            self.api_data = json.loads(api_page.text)
+        else:
+            self.response_ok = False
+            print 'Error retrieving data for {0}'.format(description)
+            print '  Status code {0}'.format(api_page.status_code)
 
-# Retrieve today and yesterday (as formatted strings) from the database
-sql = "SELECT strftime('%Y-%m-%dT%H:%M:%S', 'now'), strftime('%Y-%m-%dT%H:%M:%S', 'now', '-1 day')"
-c.execute(sql)
-sql_results = c.fetchall()
-today = sql_results[0][0]
-yesterday = sql_results[0][1]
 
-# Loop through the projects in the config file
-for project in config.sections():
+def main():
 
-    # Construct the GitHub API URL for the project
-    api_url = 'https://api.github.com/repos/' + project
+    # Read the GitHub projects from the ./config file
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    config_file_path = os.path.join(base_path, 'config')
+    config = ConfigParser.SafeConfigParser()
+    config.read(config_file_path)
 
-    # Retrieve the (json) API page
-    api_page = requests.get(api_url, verify=True)
-    if api_page.status_code != 200:
-        print 'Error retrieving API page {0}'.format(api_url)
-        print '  Status code {0}'.format(api_page.status_code)
-        sys.exit(1)
-    api_data = json.loads(api_page.text)
+    db_path = os.path.join(base_path, 'db/project_stats.db')
 
-    # Extract the number of watchers, stars, and forks
-    watchers = api_data['subscribers_count']
-    stars = api_data['stargazers_count']
-    forks = api_data['forks_count']
+    # Open the ./db/project_stats.db SQLite3 database
+    conn = sqlite3.connect(db_path)
 
-    # Count the # of commits in the last 24 hours
-    commits_url = '{0}/commits?since={1}Z'.format(api_url, yesterday)
-    commits_page = requests.get(commits_url, verify=True)
-    if commits_page.status_code != 200:
-        print 'Error retrieving Commit count page {0}'.format(commits_url)
-        print '  Status code {0}'.format(commits_page.status_code)
-        sys.exit(2)
-    commits_data = json.loads(commits_page.text)
-    commits_count = len(commits_data)
+    # Connect to the database
+    c = conn.cursor()
 
-    # Retrieve the downloads page for this project
-    download_counter = 0
-    dl_url = '{0}/releases'.format(api_url)
-    dl_page = requests.get(dl_url, verify=True)
-    if dl_page.status_code != 200:
-        print 'Error retrieving downloads count page {0}'.format(dl_url)
-        print '  Status code {0}'.format(dl_page.status_code)
-        sys.exit(3)
-    dl_data = json.loads(dl_page.text)
-
-    # Note - For each project there is an outer loop of "releases" (eg v3.6.0), with an inner loop of "assets" inside
-    # each release (with each asset having it's own download counter). eg: an .exe and a .dmg might be two assets in
-    # the same v3.6.0 release.  The .exe might have 10,000 downloads, and the .dmg might have 3,000.
-
-    # Count how many downloads have occurred (ever) for the project
-    if len(dl_data) > 0:
-        for release in dl_data:
-            for asset in release['assets']:
-                download_counter += asset['download_count']
-
-    # Print the results to stdout
-    if debug:
-        print ('{0}\n\tcommits: {1}\twatchers: {2}\tstars: {3}\tforks: {4}\t'
-               'downloads: {5}\n'.format(project, commits_count, watchers, stars, forks, download_counter))
-
-    # Add the results to the database
-    sql = ('INSERT INTO social_stats (project, time_stamp, watchers, stars, forks, commits, downloads) VALUES '
-           "('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')").format(project, today, watchers, stars, forks,
-                                                                       commits_count, download_counter)
+    # Create the SQLite3 table to store the info, if it's not already present
+    sql = ('CREATE TABLE IF NOT EXISTS social_stats (project TEXT, time_stamp TEXT, '
+           'watchers INTEGER, stars INTEGER, forks INTEGER, commits INTEGER, downloads INTEGER)')
     c.execute(sql)
     conn.commit()
 
-# Close the database connection
-c.close()
+    today = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+    yesterday = (datetime.datetime.utcnow() - datetime.timedelta(hours=24)).strftime('%Y-%m-%dT%H:%M:%S')
+
+    github = GitHubRequest()
+
+    # Loop through the projects in the config file
+    for project in config.sections():
+
+        download_counter = 0
+
+        # Query high level stats
+        github.get_stats(request_type=project, description='API page')
+        if github.response_ok:
+
+            # Extract the number of watchers, stars, and forks
+            watchers = github.api_data['subscribers_count']
+            stars = github.api_data['stargazers_count']
+            forks = github.api_data['forks_count']
+        else:
+            break
+
+        # Count the # of commits in the last 24 hours
+        github.get_stats(request_type='{0}/commits?since={1}Z'.format(project, yesterday),
+                         description='commit data')
+        if github.response_ok:
+
+            commits_count = len(github.api_data)
+        else:
+            break
+
+        # Retrieve the downloads page for this project
+        github.get_stats(request_type='{0}/releases'.format(project),
+                         description='downloads')
+        if github.response_ok:
+
+            # Note - For each project there is an outer loop of "releases" (eg v3.6.0), with an inner loop of "assets"
+            # inside each release (with each asset having it's own download counter). eg: an .exe and a .dmg might be
+            # two assets in the same v3.6.0 release.  The .exe might have 10,000 downloads, and the .dmg might
+            # have 3,000.
+
+            # Count how many downloads have occurred (ever) for the project
+            if len(github.api_data) > 0:
+                for release in github.api_data:
+                    for asset in release['assets']:
+                        download_counter += asset['download_count']
+        else:
+            break
+
+        # Print the results to stdout for debugging
+        if debug:
+            print ('{0}\n\tcommits: {1}\twatchers: {2}\tstars: {3}\tforks: {4}\t'
+                   'downloads: {5}\n'.format(project, commits_count, watchers, stars, forks, download_counter))
+
+        # Add the results to the database
+        sql = ('INSERT INTO social_stats (project, time_stamp, watchers, stars, forks, commits, downloads) VALUES '
+               "('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')").format(project, today, watchers, stars, forks,
+                                                                           commits_count, download_counter)
+        c.execute(sql)
+        conn.commit()
+
+    # Close the database connection
+    c.close()
+
+if __name__ == '__main__':
+    main()
